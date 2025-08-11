4139f26d760ad89e5a8a35e238f6dc89f167731a
Jemmy Kuria <kuriajemmy@gmail.com>
2025-08-11 23:41:12 +0300

    Made some changes

--- pages/Recommendation.py
+++ pages/Recommendation.py
@@ -387,7 +387,7 @@ class Dashboard:
                 # Prepare messages to send
                 messages_to_send = []
                 if 'name' in df.columns and 'phone_number' in df.columns:
-                    num_messages = min(10, int(max(1, details.get('numeric_value', 0) // 1000 + 1))
+                    num_messages = min(10, int(max(1, details.get('numeric_value', 0) // 1000 + 1)))
                     contacts_to_send = df[['name', 'phone_number']].head(num_messages)
                     
                     for _, row in contacts_to_send.iterrows():
